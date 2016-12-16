65bfdbb734f6addaee9ba963d87fedfb4b474680
Michael Harrison <harrim91@hotmail.co.uk>
2016-12-16 11:40:44 +0000

    Update app.rb

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,5 @@
 class TestApp < Sinatra::Base
-	register Sinatra::Flash
-  use Rack::Session::Cookie, :key => 'rack.session',
-                             :path => '/',
-                             :secret => ENV['SESSION_SECRET']
-
   get '/' do
     erb(:index)
   end
-
 end
