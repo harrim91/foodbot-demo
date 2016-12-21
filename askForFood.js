55bea73cf45cb3ec77cb1c9f62e7ab74efb92f57
Michael Harrison <harrim91@hotmail.co.uk>
2016-12-21 15:23:32 +0000

    starts to separate out options

--- askForFood.js
+++ askForFood.js
@@ -6,19 +6,19 @@ exports.handler = (event, context, callback) => {
   var responseJSON = {
     "response": "",
     "continue": true,
-    "customPayload": "",
+    "customPayload": null,
     "quickReplies": null
-  }
+  };
 
   request(`${openFoodFactsAPIURL}${event.reply}.json`, function(error, response, body) {
 
     var json = JSON.parse(body);
-    responseJSON.customPayload = json;
     
     if(error || json.status === '0' || json.product._id === '') {
         responseJSON.response = `Oops, I can't find that product, want to search again?`;
         responseJSON.quickReplies = ['Yes', 'No'];
     } else {
+        responseJSON.customPayload = json.product;
         responseJSON.response = `Ah! `;
         if (json.product.generic_name && json.product.generic_name !== '') {
             if (json.product.brands && json.product.brands !== '') responseJSON.response += `${json.product.brands} `;
--- energy.js
+++ energy.js
@@ -1,29 +1,22 @@
-var promise = require('q');
-
 exports.handler = (event, context, callback) => {
 
   var responseJSON = {
     "response": "",
     "continue": true,
-    "customPayload": "",
+    "customPayload": null,
     "quickReplies": null
+  };
+
+  var product = event.customPayload;
+  console.log(product);
+
+  if(product.nutriments.energy && product.nutriments.energy !== '') {
+    responseJSON.response += `These have ${product.nutriments.energy}${product.nutriments.energy_unit} of energy per ${product.nutrition_data_per}. `;
+  } else {
+    responseJSON.response += `I can't find any information about that! `;
   }
+  responseJSON.response += `Want to search again?`;
+  responseJSON.quickReplies = ['Yes', 'No'];
 
-  setResponse(event.customPayload, responseJSON)
-    .then(function() {
-      callback(null, responseJSON);
-    })
-    .done();
+  callback(null, responseJSON);
 };
-
-function setResponse(data, responseJSON) {
-  return promise.fcall(function(data, responseJSON) {
-    if(data.product.nutriments.energy && data.product.nutriments.energy !== '') {
-      responseJSON.response += `These have ${data.product.nutriments.energy}${data.product.nutriments.energy_unit} of energy per ${data.product.nutrition_data_per}. `;
-    } else {
-        responseJSON.response += `I can't find any information about that! `;
-    }
-    responseJSON.response += `Want to search again?`;
-    responseJSON.quickReplies = ['Yes', 'No'];
-  });
-}
